36c21693fdbb240c2826785205cecc1da71ffc38
Daniil Panov <daniil_panov2005@mail.ru>
2023-05-13 03:13:24 +0300

    feat: uniqulizer is better that it was

--- uniqulizer.py
+++ uniqulizer.py
@@ -13,12 +13,23 @@ def uniqulize(filepath, output='output.csv'):
                 f.write(row)
 
 
+def uniqulize_by_df(filepath, output='output.csv', header_row=None):
+    import pandas
+    df = pandas.read_csv(filepath, header=header_row)
+    df.drop_duplicates().to_csv(output, index=False, header=(header_row == 0))
+
+
 if __name__ == '__main__':
     # uniqulizer
     import os.path
 
-    path: str = "reviews.csv"
+    path = "reviews.csv"
+    f_out = "output.csv"
+    header = False
     while not os.path.exists(os.path.abspath(path)):
-        path = input("Please, input the filepath: ")
+        path = input("Please, type the filepath: ")
+        header = input("Have this file header? [yes/no]: ").lower() in ('yes', 'y')
+        f_out = input("Please, type the output file or just press enter to choose default filepath - ./output.csv: ") \
+                or "output.csv"
 
-    uniqulize(path)
+    uniqulize_by_df(path, f_out, 0 if header else None)
