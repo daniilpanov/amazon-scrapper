1f5a21378bf9865ed5f35cae76d80f98af863d81
Daniel <daniil_panov2005@mail.ru>
2023-08-02 03:00:23 +0300

    fix: writing data

--- main.py
+++ main.py
@@ -271,7 +271,6 @@ def process_data():
                 if not item_parser or not item_parser.find(attrs={'data-hook': 'review'}) \
                         or item_parser.find('div', class_='a-divider-section') \
                         or item_parser.find('h3', attrs={'data-hook': 'dp-global-reviews-header'}):
-                    print(item_parser)
                     continue
                 # Country & Date
                 review_date_raw = item_parser.find('span', attrs={'data-hook': 'review-date'})
