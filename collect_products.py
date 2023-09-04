6766ea44a1b09adfa086876b143b9897aee6bbe9
Daniil Panov <daniil_panov2005@mail.ru>
2023-09-05 02:02:48 +0300

    feat::unchecked: base of collect_products inserted

--- collect_products.py
+++ collect_products.py
@@ -1,4 +1,9 @@
+import urllib
+from json import JSONDecoder
+
 from functions import chrome_init
+
+# TODO: queues system (writer, products_info, reviews)
 
 
 def collect_asins(query, market_niche=None):
@@ -41,18 +46,24 @@ def collect_asins(query, market_niche=None):
         webdriver.sleep(10)
         webdriver.type('#twotabsearchtextbox,#nav-bb-search', query)
         webdriver.submit('#twotabsearchtextbox,#nav-bb-search')
-    except:
-        try:
-            webdriver.driver.close()
-        finally:
-            return False
 
-    # TODO: COLLECTING
-
-    try:
-        webdriver.driver.close()
-    finally:
+        webdriver.click('a.s-pagination-item.s-pagination-button')
+        webdriver.sleep(1)
+        params_str = webdriver.get_current_url().split('?')[1]
+        args = dict(list(tuple(map(lambda x: urllib.parse.quote(urllib.parse.unquote(x)),
+                                   map(lambda x: x.replace('+', ' '), item.split('='))))
+                         for item in params_str.split('&')))
+        webdriver.activate_jquery()
+        q = '?' + '&'.join('='.join(map(str, keyval)) for keyval in args.items())
+        result = webdriver.execute_script(f"$.post(\"https://www.amazon.com/s/query{q}\", "
+                                          "{" + '",'.join([':"'.join(map(str, keyval)) for keyval in args.items()]) + ""
+                                          "\"}, null, 'text');")
+        asin_write(result, args['pageNumber'])
         return True
+    except:
+        return False
+    finally:
+        webdriver.driver.close()
 
 
 def collect_products_info():
@@ -68,8 +79,19 @@ def writer(item, data):
     pass
 
 
-def asin_write(data):
-    pass
+def asin_write(data, page):
+    decoder = JSONDecoder()
+    raw_data = list(map(lambda s: decoder.decode(s.strip()), filter(lambda x: x, data.strip().split('&&&'))))
+    count_all_products = 0
+    rows = []
+
+    with open('products.list', 'a') as f:
+        for item in raw_data:
+            if count_all_products > 0 and count_all_products // 48 + 1 < page:
+                return count_all_products
+            if 'data-main-slot:search-result-' in item[1]:
+                rows.append(item[2]['asin'] + '\n')
+        f.writelines(rows)
 
 
 def product_info_write(data):
