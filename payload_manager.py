45f329e89397702e94eaddd274b18861464c6843
Daniel <daniil_panov2005@mail.ru>
2023-11-20 19:48:29 +0300

    feat: added collection of products

--- payload_manager.py
+++ payload_manager.py
@@ -3,12 +3,15 @@ from multiprocessing import Pipe
 from queue import Queue
 from threading import Thread
 
+import collect_products
 import collect_reviews
 import state
 
-processes_pool = ProcessPoolExecutor(4)
+reviews_processes_pool = ProcessPoolExecutor(4)
+products_processes_pool = ProcessPoolExecutor(1)
 reader, writer = Pipe(False)
-planned_asins = set()
+reviews_planned_asins = set()
+products_planned_asins = set()
 
 
 def init():
@@ -30,14 +33,28 @@ def extend(asins, callback, callback_args=None):
         callback_args = []
     for asin in asins:
         append(asin, callback, callback_args)
+    add_products_info_collect(asins)
 
+
+def add_products_info_collect(asins):
+    feat = products_processes_pool.submit(collect_products.collect_products_info, asins, reader)
+
+    def decrement(res):
+        collected_asins = res.result()
+        if not collected_asins:
+            return add_products_info_collect(asins)
+        not_collected_asins = asins - collected_asins
+        if len(not_collected_asins) > 0:
+            add_products_info_collect(not_collected_asins)
+
+    feat.add_done_callback(decrement)
 
 def append(asin, callback, callback_args):
     if state.get_asin(asin) == -1:
         return callback(*callback_args, (asin,))
-    feat = processes_pool.submit(collect_reviews.main, asin, reader)
+    feat = reviews_processes_pool.submit(collect_reviews.main, asin, reader)
     def decrement(_):
-        planned_asins.remove(asin)
+        reviews_planned_asins.remove(asin)
     def cbk(res):
         if res.result():
             callback(*callback_args, (asin,))
@@ -49,8 +66,8 @@ def append(asin, callback, callback_args):
 
 
 def close_all():
-    for _ in range(len(planned_asins)):
+    for _ in range(len(reviews_planned_asins)):
         writer.send(False)
-    processes_pool.shutdown(True)
+    reviews_processes_pool.shutdown(True)
     writer.close()
     reader.close()
