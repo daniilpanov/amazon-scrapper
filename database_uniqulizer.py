d8d5a1b27e076ffa15fd62a5dd01eca8a4e67e59
Daniil Panov <daniil_panov2005@mail.ru>
2023-12-01 12:29:12 +0300

    feat: upgrade the database_uniqulizer

--- database_uniqulizer.py
+++ database_uniqulizer.py
@@ -1,19 +1,26 @@
 import database as db
 
-reviews_ready = [el['review_id'] for el in db.db()['temp_customer_reviews'].find({})]
-data = db.db()['customer_reviews'].find({'review_id': {'$nin': list(reviews_ready)}})
+collection = input('Please enter the collection name: ') or 'customer_reviews'
+temp_collection = input('Please enter the temp collection name: ') or 'temp_' + collection
+uniq_col = input('Enter the unique column: ') or 'review_id'
+
+els_ready = [el[uniq_col] for el in db.db()[temp_collection].find({})]
+data = db.db()[collection].find({uniq_col: {'$nin': list(els_ready)}} if els_ready else {})
 new_data = {}
 
 for item in data:
-    new_data[item['review_id']] = item
-db.db()['temp_customer_reviews'].insert_many(new_data.values())
-# asin = None
-# for item in data:
-#     if item['asin'] != asin:
-#         asin = item['asin']
-#         print(asin)
-#     try:
-#         db.db()['temp_customer_reviews'].insert_one(item)
-#     except:
-#         pass
+    new_data[item[uniq_col]] = item
+try:
+    db.db()[temp_collection].insert_many(new_data.values())
+except:
+    pass
+asin = None
+for item in data:
+    if item.get('asin') != asin:
+        asin = item['asin']
+        print(asin)
+    try:
+        db.db()[temp_collection].insert_one(item)
+    except:
+        pass
 
