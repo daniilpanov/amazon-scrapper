6bc3d4e74942616e1089e778650cd8e0e7e542fb
Daniil Panov <daniil_panov2005@mail.ru>
2023-12-15 01:21:41 +0300

    feat: checked prototype of the get_asins_data function

--- telegram_bot2.py
+++ telegram_bot2.py
@@ -31,6 +31,8 @@ def log(*args, **kwargs):
 
 
 def new_process(script, *args, stdin=None, stdout=None, stderr=None, **kwargs):
+    log(f'new_process: {script}. ASINs:', kwargs.get('asins'), ';list_name:', kwargs.get('list_name'))
+    return
     return Popen(
         [sys.executable, script + '.py', *args, *list(key + '=' + kwargs[key] for key in kwargs)],
         stdin=stdin or sys.stdin, stdout=stdout or sys.stdout, stderr=stderr or sys.stderr,
@@ -47,7 +49,6 @@ def receive_message(msg: types.Message):
 
 
 def auth(msg: types.Message):
-    print('ok!')
     return msg.from_user.id in auth_users
 
 
@@ -57,23 +58,33 @@ def get_asins_data(msg: types.Message, **kwargs):
     receive_message(msg)
     if msg.text in ('/close', '/stop', '/quit'):
         return send_msg(msg.from_user.id, 'Cancel')
-    asins_raw = msg.text.replace('/get_asins_data ', '').strip()
+    asins_raw = msg.text.replace('/get_asins_data', '').strip()
     if asins_raw:
         asins = set(get_all_asins_from_text(asins_raw))
         if asins:
             if 'list_name' in kwargs:
                 new_process('main_collect_all', asins=''.join(asins), list_name=kwargs['list_name'])
-                return send_msg(msg.from_user.id, 'Process started. We\'ll notify you when it is completed')
+                return send_msg(
+                    msg.from_user.id,
+                    f'Process started. We\'ll notify you when it is completed. List name: {kwargs["list_name"]}',
+                )
             kwargs.update({'asins': asins})
-            return bot.register_next_step_handler(send_msg(msg.from_user.id, 'Enter the list name:'), get_asins_data, **kwargs)
-        if 'asin' in kwargs:
-            log('')
+            return bot.register_next_step_handler(send_msg(
+                msg.from_user.id, 'Enter the list name:'),
+                get_asins_data, **kwargs,
+            )
+        if 'asins' in kwargs:
             new_process('main_collect_all', asins=''.join(kwargs['asins']), list_name=asins_raw)
-            return send_msg(msg.from_user.id, 'Process started. We\'ll notify you when it is completed')
+            return send_msg(
+                msg.from_user.id,
+                f'Process started. We\'ll notify you when it is completed. List name: {asins_raw}',
+            )
         kwargs.update({'list_name': asins_raw})
-        return bot.register_next_step_handler(send_msg(msg.from_user.id, 'Enter the ASINs list:'), get_asins_data, **kwargs)
-    return bot.register_next_step_handler(send_msg(msg.from_user.id, 'Please enter the ASINs list:'), get_asins_data)
-
+        return bot.register_next_step_handler(send_msg(
+            msg.from_user.id,
+            f'List name: {asins_raw}. Enter the ASINs list:'), get_asins_data, **kwargs,
+        )
+    return bot.register_next_step_handler(send_msg(msg.from_user.id, 'Enter the ASINs list:'), get_asins_data)
 
 
 def export_asins(msg: types.Message, *args, **kwargs):
@@ -93,7 +104,7 @@ def unknown(msg: types.Message):
 
 
 CMDs = {
-    'get_all_asins_data': get_asins_data,
+    'get_asins_data': get_asins_data,
     'export_asins': export_asins,
     'import_asins': import_asins,
     'delete_asins': delete_asins,
