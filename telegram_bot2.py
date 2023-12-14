4b3e0aad4cf8ef124cfa689bbb1ba706b9bd8be3
Daniil Panov <daniil_panov2005@mail.ru>
2023-12-14 17:28:59 +0300

    tmp

--- telegram_bot2.py
+++ telegram_bot2.py
@@ -2,6 +2,7 @@
 import os
 
 import bottle
+import colorama
 import pandas as pd
 from subprocess import Popen
 from threading import Thread
@@ -20,56 +21,68 @@ bot = telebot.TeleBot('6907121969:AAFxNOUoBwata5M_YEXwGj_dGanLN6ct1gc', parse_mo
 auth_users = {320753905, 1428909514}
 processes = set()
 
+DEBUG = True
+
+
+def log(*args, **kwargs):
+    if DEBUG:
+        print(colorama.Back.GREEN, *args, colorama.Back.RESET, **kwargs)
+
+
+def send_msg(user_id, message, *args, **kwargs):
+    log(f'Message to {user_id}: "{message}"')
+    return bot.send_message(user_id, message, *args, **kwargs)
+
 
 def receive_message(msg: types.Message):
-    pass
+    log(f'Receive message from {msg.from_user.id}:', msg.text)
 
 
 def auth(msg: types.Message):
     return msg.from_user.id in auth_users
 
 
-def get_all_asins_data(msg: types.Message):
+def get_all_asins_data(msg: types.Message, *args, **kwargs):
     pass
 
 
-def export_asins(msg: types.Message):
+def export_asins(msg: types.Message, *args, **kwargs):
     pass
 
 
-def import_asins(msg: types.Message):
+def import_asins(msg: types.Message, *args, **kwargs):
     pass
 
 
-def delete_asins(msg: types.Message):
+def delete_asins(msg: types.Message, *args, **kwargs):
     pass
 
 
 def buttons():
-    pass
+    keyboard = types.ReplyKeyboardMarkup()
+    for cmd in CMDs:
+        key = types.InlineKeyboardButton(text=CMDs[cmd][1], callback_data=cmd)
+        keyboard.add(key)
+
+    return keyboard
 
 
+@bot.callback_query_handler(func=lambda *args, **kwargs: True)
 def process_buttons():
     pass
 
 
 CMDs = {
-    'get_all_asins_data': get_all_asins_from_text,
-    'export_asins': export_asins,
-    'import_asins': import_asins,
-    'delete_asins': delete_asins,
+    'get_all_asins_data': (get_all_asins_from_text, 'Get All ASINs Data'),
+    'export_asins': (export_asins, 'Export ASINs'),
+    'import_asins': (import_asins, 'Import ASINs'),
+    'delete_asins': (delete_asins, 'Delete ASINs'),
 }
 
-for cmd in CMDs:
-    dcmd = CMDs[cmd]
-    if type(dcmd) in (list, tuple, set, frozenset):
-        func, auth_needle = dcmd
-        if auth_needle:
-            bot.register_message_handler(callback=func, commands=[cmd], func=auth)
-        else:
-            bot.register_message_handler(callback=func, commands=[cmd])
-    else:
+
+if __name__ == '__main__':
+    for cmd in CMDs:
+        dcmd = CMDs[cmd]
         bot.register_message_handler(callback=dcmd, commands=[cmd], func=auth)
 
-
-
+    bot.infinity_polling()
