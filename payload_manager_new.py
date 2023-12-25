d33644b80dbd9d99a0e4d291fe73a21dcdb9405f
Daniil Panov <daniil_panov2005@mail.ru>
2023-12-25 04:05:18 +0300

    fix: new function in payload manager - add 'user' arg

--- payload_manager_new.py
+++ payload_manager_new.py
@@ -28,8 +28,8 @@ def add_deals_task(name, user_id):
     processes_waiters.submit(add_task, 'collect_all_deals', sheet_name=name, user=user_id)
 
 
-def add_asins_nearby_task(asin):
-    processes_waiters.submit(add_task, 'collect_asins_nearby', asin=asin)
+def add_asins_nearby_task(asin, user_id):
+    processes_waiters.submit(add_task, 'collect_asins_nearby', asin=asin, user=user_id)
 
 
 # Добавление процесса и ожидание завершения (ф-я запускается в отдельном потоке)
