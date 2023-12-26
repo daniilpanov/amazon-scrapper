e1a3bd691d3545edee42b4fe6c8bb211679b8f71
Daniil Panov <daniil_panov2005@mail.ru>
2023-12-26 22:22:41 +0300

    fix::unchecked: change encoding

--- telegram_bot.py
+++ telegram_bot.py
@@ -216,7 +216,7 @@ def _import_asins(user_id, document, location):
         'ai_highlights.aspects_new': ai_highlights_aspects_new_mapping,
     }
     try:
-        string = document.decode('utf-8')
+        string = document.decode('utf-16')
         lines = string.splitlines()
         header = lines[0]
         delimiter = None
