1b180ae8a5ac2dbffc404bfa569b18dff40e2258
Daniil Panov <daniil_panov2005@mail.ru>
2023-12-28 20:20:43 +0300

    debug::on

--- telegram_bot.py
+++ telegram_bot.py
@@ -202,6 +202,8 @@ def ai_highlights_aspects_new_mapping(head: list[str], data: DataFrame):
         'product_url', 'date', 'country', 'name', 'title',
         'content', 'rating', 'helpful', 'options', 'scrap_datetime',
     ] if col in head)
+    log(cols_for_drop)
+    log(head)
     data.drop(cols_for_drop, axis=1)
     new_data = pandas.melt(data, ['review_id', 'asin'], var_name='Aspect', value_name='Value')
     new_data.dropna(subset=['Aspect', 'Value'])
