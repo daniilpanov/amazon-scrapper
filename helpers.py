4533fc63166f3e9bc3ded41b44e9ed7427ee6b7e
Daniil Panov <daniil_panov2005@mail.ru>
2023-12-29 20:34:59 +0300

    hotfix: asins_nearby

--- helpers.py
+++ helpers.py
@@ -25,7 +25,7 @@ def log(*args, **kwargs):
 def parse_args(argv: list[str]):
     params_dict = {}
     for arg in argv:
-        row = arg.split('=')
+        row = arg.split('=', 1)
         if len(row) > 1:
             params_dict[row[0]] = row[1]
     return params_dict
