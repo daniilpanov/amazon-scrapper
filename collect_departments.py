1c48df2818cabe716fddd1a7178bedd4e1477e6f
Daniil Panov <daniil_panov2005@mail.ru>
2024-02-09 05:59:41 +0300

    fix: departments depth

--- collect_departments.py
+++ collect_departments.py
@@ -174,9 +174,8 @@ def recursive_tree(tree: Level, wd: WebDriver, xsh: XSheet, name_only=None):
     if not tree.all_items_preloaded:
         wd.get(tree.link)
         soup = BeautifulSoup(wd.get_page_source(), features='html.parser')
-        view_tree = soup.find(role='tree')
-        group = view_tree.find(role='group')
-        tree.is_last_group = group is None or tree.name in group.text and not name_only
+        group = soup.find(role='tree').find(role='group')
+        tree.is_last_group = bool(group.select('[role="treeitem"] > span') if group else False)
         if not tree.is_last_group:
             links_a = group.find_all('a')
             for a_tag in links_a or []:
