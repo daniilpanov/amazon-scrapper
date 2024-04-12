27c360ea0cac23c6f5cdc2731f3f13b76bfed152
Daniil Panov <daniil_panov2005@mail.ru>
2024-04-13 01:36:59 +0300

    fix: universal script.js

--- web/js/script.js
+++ web/js/script.js
@@ -1,5 +1,6 @@
 // const base_url = 'http://195.201.194.213:8830';
-const base_url = 'http://localhost:8830';
+// const base_url = 'http://localhost:8830';
+const base_url = '';
 
 function add_task(script, task_alias, data, callback = null) {
     let url = base_url + '/tasks/add/' + script;
