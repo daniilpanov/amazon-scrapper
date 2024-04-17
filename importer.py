7071c5092bf4c71a8bfe7ae4cdb6007ec230aa3e
Daniil Panov <daniil_panov2005@mail.ru>
2024-04-17 04:04:51 +0300

    fix: finishgit push!

--- importer.py
+++ importer.py
@@ -32,7 +32,6 @@ async def import_report(request: Request, document: UploadFile = File(...)):
     if not auth_token or sha256(auth_token.encode('utf-8')).hexdigest() != api_key_hashed or auth_token != api_key:
         raise HTTPException(status_code=403, detail='Invalid API KEY')
     df = pd.read_csv(io.BytesIO(await document.read()), header=0, index_col=None, delimiter=';')
-    print(df)
     try:
         database.db('Keywords')['Amazon_keyword_tracker'].insert_many(list(df.T.to_dict().values()))
     except BulkWriteError:
