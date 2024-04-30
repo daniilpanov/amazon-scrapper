67f0a4cc7b98897c5d7ecca1f0f1f6962a9c1303
Daniil Panov <daniil_panov2005@mail.ru>
2024-04-30 13:48:35 +0300

    fix: deep-r

--- collect_reviews/deep_collect_reviews.py
+++ collect_reviews/deep_collect_reviews.py
@@ -1,6 +1,8 @@
 from database import spec_db
 from .reserve_async_collect_reviews import collect
 
-all_top_phrases = spec_db('ai_highlights')['top_phrases'].find({'asin': 'B01JQZUMXY'}, sort=('count', 1))
-for keyword in all_top_phrases:
-    collect(None, None, 'B01JQZUMXY', keyword, 'amazon.com', 0, True)
+
+def start():
+    all_top_phrases = spec_db('ai_highlights')['top_phrases'].find({'asin': 'B01JQZUMXY'}, sort=(('count', 1),))
+    for keyword in all_top_phrases:
+        collect(None, None, 'B01JQZUMXY', keyword, 'amazon.com', 0, True)
