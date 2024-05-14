b387f562f9e4fddd9209018853a3e7c71e96cb11
Daniil Panov <daniil_panov2005@mail.ru>
2024-05-14 23:38:04 +0300

    fix: functions names

--- server_service/server.py
+++ server_service/server.py
@@ -139,9 +139,16 @@ async def get_task_req(task_id: str, with_header: bool = True, body_only: bool =
 
 
 @app.get('/tasks/get')
-async def get_tasks_req():
-    res = list(tasks.get_all_tasks())
+@app.get('/tasks/get/{script}')
+async def get_tasks_req(script: str | None = None):
+    res = list(tasks.get_all_tasks({'script': script} if script else None))
     return JSONResponse(json.loads(json_util.dumps(res)))
+
+
+@app.get('/tasks/filter')
+async def filter_tasks_req(_filter: str):
+    prepared_filter = json.loads(_filter)
+    return JSONResponse(json.loads(json_util.dumps(tasks.get_all_tasks(prepared_filter))))
 
 
 @app.post('/tasks/acquire/{script}/{header_id}/{task_id}')
@@ -159,7 +166,7 @@ async def release_task_req(task_id: str):
 
 @app.get('/tasks/get_available')
 @app.get('/tasks/get_available/{script}')
-async def get_tasks_req(script: str | None = None):
+async def get_available_tasks_req(script: str | None = None):
     res = list(tasks.get_all_tasks(({'script': script} if script else {}) | {'taskLock': {'$exists': False}}))
     return JSONResponse(json.loads(json_util.dumps(res)))
 
