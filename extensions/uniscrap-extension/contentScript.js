b261b5f45971fa2bb92bd0888dc3536cfad334a5
Daniil Panov <daniil_panov2005@mail.ru>
2024-07-12 17:59:00 +0300

    debug::on [uniscrap.ext]

--- extensions/uniscrap-extension/contentScript.js
+++ extensions/uniscrap-extension/contentScript.js
@@ -80,86 +80,30 @@ async function main() {
         return;
     }
     curr_limit = tabs_limit - tabs_count;
-    const res = await fetch('http://195.201.194.213:8832/tasks/get_available');
-    const data = await res.json();
-    let all_extensions, found;
-    for (let i in data) {
-        console.log('Available space left:', curr_limit);
-        if (curr_limit <= 0) {
-            console.log('exit from cycle! limit!');
-            break;
-        }
-        switch (data[i].script) {
-            case '100asins':
-                all_extensions = await chrome.management.getAll();
-                found = false;
-                all_extensions.forEach((async (ext) => {
-                    if (ext.name.toLowerCase().includes('amazon 100 asins scraper')) {
-                        found = true;
-                        const msg = await chrome.runtime.sendMessage(ext.id, {...data[i].data, task_id: data[i]._id, header_id: data[i].header_id});
-                        console.log('Message sent to', ext.name, `[${ext.id}]`);
-                        switch (msg) {
-                            case 'OK':
-                                console.log('task started:', data[i]);
-                                --curr_limit;
-                                break;
-                            case 'fail':
-                                console.log('task can not be started due to unknown error:', data[i]);
-                                break;
-                            case 'busy':
-                                console.log('task is busy:', data[i]);
-                                break;
-                        }
-                    }
-                }));
-                if (!found) {
-                    console.log('No extensions found for script products!');
-                }
+    try {
+        const res = await fetch('http://195.201.194.213:8832/tasks/get_available');
+        const data = await res.json();
+        let all_extensions, found;
+        for (let i in data) {
+            console.log('Available space left:', curr_limit);
+            if (curr_limit <= 0) {
+                console.log('exit from cycle! limit!');
                 break;
-            case 'h10':
-                setTimeout(h10scrap, 500, data[i]);
-                --curr_limit;
-                break;
-            case 'products':
-                all_extensions = await chrome.management.getAll();
-                found = false;
-                all_extensions.forEach((async (ext) => {
-                    if (ext.name.toLowerCase().includes('amazon products scraper')) {
-                        found = true;
-                        const msg = await chrome.runtime.sendMessage(ext.id, {root: self, task: data[i]});
-                        console.log('Message sent to', ext.name, `[${ext.id}]`);
-                        switch (msg) {
-                            case 'OK':
-                                console.log('task started:', data[i]);
-                                --curr_limit;
-                                break;
-                            case 'fail':
-                                console.log('task can not be started due to unknown error:', data[i]);
-                                break;
-                            case 'busy':
-                                console.log('task is busy:', data[i]);
-                                break;
-                        }
-                    }
-                }));
-                if (!found) {
-                    console.log('No extensions found for script products!');
-                }
-                break;
-            case 'bsr':
-                if ((data[i].stage || 0) >= 2) {
-                    break;
-                }
-                all_extensions = await chrome.management.getAll();
-                found = false;
-                all_extensions.forEach(ext => {
-                    if (ext.name.toLowerCase().includes('amazon bsr scraper')) {
-                        found = true;
-                        chrome.runtime.sendMessage(ext.id, {root: self, task: data[i]}, (res) => {
-                            switch (res) {
+            }
+            switch (data[i].script) {
+                case '100asins':
+                    all_extensions = await chrome.management.getAll();
+                    found = false;
+                    all_extensions.forEach((async (ext) => {
+                        if (ext.name.toLowerCase().includes('amazon 100 asins scraper')) {
+                            found = true;
+                            const msg = await chrome.runtime.sendMessage(ext.id, {...data[i].data, task_id: data[i]._id, header_id: data[i].header_id});
+                            console.log('Message sent to', ext.name, `[${ext.id}]`);
+                            switch (msg) {
                                 case 'OK':
                                     console.log('task started:', data[i]);
                                     --curr_limit;
+                                    console.log(curr_limit);
                                     break;
                                 case 'fail':
                                     console.log('task can not be started due to unknown error:', data[i]);
@@ -168,18 +112,81 @@ async function main() {
                                     console.log('task is busy:', data[i]);
                                     break;
                             }
-                        })
-                        console.log('Message sent to', ext.name, `[${ext.id}]`);
+                        }
+                    }));
+                    if (!found) {
+                        console.log('No extensions found for script products!');
                     }
-                });
-                if (!found) {
-                    console.log('No extensions found for script BSR!');
-                }
-                break;
-            default:
-                console.log('Unknown script:', data[i].script);
-                break;
+                    break;
+                case 'h10':
+                    setTimeout(h10scrap, 500, data[i]);
+                    --curr_limit;
+                    break;
+                case 'products':
+                    all_extensions = await chrome.management.getAll();
+                    found = false;
+                    all_extensions.forEach((async (ext) => {
+                        if (ext.name.toLowerCase().includes('amazon products scraper')) {
+                            found = true;
+                            const msg = await chrome.runtime.sendMessage(ext.id, {root: self, task: data[i]});
+                            console.log('Message sent to', ext.name, `[${ext.id}]`);
+                            switch (msg) {
+                                case 'OK':
+                                    console.log('task started:', data[i]);
+                                    --curr_limit;
+                                    console.log(curr_limit);
+                                    break;
+                                case 'fail':
+                                    console.log('task can not be started due to unknown error:', data[i]);
+                                    break;
+                                case 'busy':
+                                    console.log('task is busy:', data[i]);
+                                    break;
+                            }
+                        }
+                    }));
+                    if (!found) {
+                        console.log('No extensions found for script products!');
+                    }
+                    break;
+                case 'bsr':
+                    if ((data[i].stage || 0) >= 2) {
+                        break;
+                    }
+                    all_extensions = await chrome.management.getAll();
+                    found = false;
+                    all_extensions.forEach(ext => {
+                        if (ext.name.toLowerCase().includes('amazon bsr scraper')) {
+                            found = true;
+                            chrome.runtime.sendMessage(ext.id, {root: self, task: data[i]}, (res) => {
+                                switch (res) {
+                                    case 'OK':
+                                        console.log('task started:', data[i]);
+                                        --curr_limit;
+                                        console.log(curr_limit);
+                                        break;
+                                    case 'fail':
+                                        console.log('task can not be started due to unknown error:', data[i]);
+                                        break;
+                                    case 'busy':
+                                        console.log('task is busy:', data[i]);
+                                        break;
+                                }
+                            })
+                            console.log('Message sent to', ext.name, `[${ext.id}]`);
+                        }
+                    });
+                    if (!found) {
+                        console.log('No extensions found for script BSR!');
+                    }
+                    break;
+                default:
+                    console.log('Unknown script:', data[i].script);
+                    break;
+            }
         }
+    } catch (e) {
+        console.log(e);
     }
     interval_id = setTimeout(main, 10000);
 }
