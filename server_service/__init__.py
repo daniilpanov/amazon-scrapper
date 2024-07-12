c8f05303424ab93ad152c14e85925b89e145ce45
Daniil Panov <daniil_panov2005@mail.ru>
2024-07-13 01:44:28 +0300

    feat: [server_service] add logger for 422 error

--- server_service/__init__.py
+++ server_service/__init__.py
@@ -2,7 +2,12 @@ import importlib
 import os
 
 import fastapi
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import ORJSONResponse
 from starlette.middleware.cors import CORSMiddleware
+from starlette.requests import Request
+from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
 app = fastapi.FastAPI()
 app.add_middleware(
@@ -12,6 +17,21 @@ app.add_middleware(
     allow_methods=['*'],
     allow_headers=['*'],
 )
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    for h in request.headers:
+        if h.lower() == 'content-type':
+            print(await request.json())
+            break
+    else:
+        print(await request.body())
+    return ORJSONResponse(
+        status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
+    )
+
 
 modules = []
 
