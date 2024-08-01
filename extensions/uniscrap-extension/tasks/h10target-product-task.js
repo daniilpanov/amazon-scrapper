555a0496f58fa1cb112c08a190f41fac58017223
Daniil Panov <daniil_panov2005@mail.ru>
2024-08-01 17:34:06 +0300

    fix: [uniscrap.h10.target.ext] brand collecting

--- extensions/uniscrap-extension/tasks/h10target-product-task.js
+++ extensions/uniscrap-extension/tasks/h10target-product-task.js
@@ -2,7 +2,7 @@
 chrome.runtime.onMessage.addListener((task, sender, sendResponse) => {
     sendResponse('OK');  // Success
     waitForElement('#feature-bullets, #productFactsDesktop_feature_div div[aria-expanded]', (els, c) => {
-        let description = null, title = null, brand;
+        let description = null, title = null, brand = null;
 
         for (const el_id of ['titleSection', 'title', 'productTitle']) {
             title = document.getElementById(el_id)?.innerText || null;
@@ -15,9 +15,14 @@ chrome.runtime.onMessage.addListener((task, sender, sendResponse) => {
             description = els[0]?.innerText || null;
         }
 
-        brand = document.querySelector('.po-brand')?.innerText.split(/\s+/) || null;
-        if (brand && brand.length&& brand.length === 2) {
-            brand = brand[1];
+        const raw_brand = document.querySelector('.po-brand')?.innerText.split(/\s+/) || null;
+        if (raw_brand && raw_brand.length) {
+            for (const i in raw_brand) {
+                if (raw_brand[i].toLowerCase() === 'brand' && i + 1 < raw_brand.length) {
+                    brand = raw_brand[i + 1];
+                    break;
+                }
+            }
         }
 
         console.log(chrome.runtime.sendMessage(
