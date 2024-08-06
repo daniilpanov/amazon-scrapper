d30e2682a82bbda8b661c0c9327af52a53d6360a
Daniil Panov <daniil_panov2005@mail.ru>
2024-08-07 00:20:50 +0300

    feat: [importer.service] add tiktoklinks

--- keywords_data_importer_service/importer.py
+++ keywords_data_importer_service/importer.py
@@ -51,6 +51,20 @@ async def import_tiktok(request: Request, document: UploadFile = File(...)):
     return Response(status_code=204)
 
 
+@app.post('/import/tiktoklinks')
+async def import_tiktok(request: Request, document: UploadFile = File(...)):
+    auth_token = request.headers.get('Authorization')
+    if not auth_token or sha256(auth_token.encode('utf-8')).hexdigest() != api_key_hashed or auth_token != api_key:
+        raise HTTPException(status_code=403, detail='Invalid API KEY')
+    df = pd.read_csv(io.BytesIO(await document.read()), header=0, index_col=None, delimiter=';')
+    try:
+        # db_mongo.db('Keywords')['Amazon_keyword_tracker'].insert_many(list(df.T.to_dict().values()))
+        db_mongo.db('Keywords')['tiktoklinks'].insert_many(list(df.T.to_dict().values()), ordered=False)
+    except BulkWriteError:
+        pass
+    return Response(status_code=204)
+
+
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(app, host='0.0.0.0', port=8831)
