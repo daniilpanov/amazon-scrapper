f9526efd837e4998d55247ca088727c61ff00bb4
Daniil Panov <daniil_panov2005@mail.ru>
2024-08-08 19:29:07 +0300

    feat: [importer.service] add universal endpoint for loading into collections

--- keywords_data_importer_service/importer.py
+++ keywords_data_importer_service/importer.py
@@ -23,44 +23,16 @@ api_key = '2dBtEL2DRjO0AAQqaKLWEAN4xr4XTaqwSRyUXepJRDYEiseSs7JzfGTVLc3b0poS'
 api_key_hashed = sha256(api_key.encode('utf-8')).hexdigest()
 
 
-# TODO: add universal method
-@app.post('/import/report')
-async def import_report(request: Request, document: UploadFile = File(...)):
+@app.post('/import/{collection}')
+async def import_tiktok(request: Request, collection: str, document: UploadFile = File(...)):
+    if collection == 'report':
+        collection = 'keyword_tracker'
     auth_token = request.headers.get('Authorization')
     if not auth_token or sha256(auth_token.encode('utf-8')).hexdigest() != api_key_hashed or auth_token != api_key:
         raise HTTPException(status_code=403, detail='Invalid API KEY')
     df = pd.read_csv(io.BytesIO(await document.read()), header=0, index_col=None, delimiter=';')
     try:
-        # db_mongo.db('Keywords')['Amazon_keyword_tracker'].insert_many(list(df.T.to_dict().values()))
-        db_mongo.db('Keywords')['keyword_tracker'].insert_many(list(df.T.to_dict().values()), ordered=False)
-    except BulkWriteError:
-        pass
-    return Response(status_code=204)
-
-
-@app.post('/import/tiktok')
-async def import_tiktok(request: Request, document: UploadFile = File(...)):
-    auth_token = request.headers.get('Authorization')
-    if not auth_token or sha256(auth_token.encode('utf-8')).hexdigest() != api_key_hashed or auth_token != api_key:
-        raise HTTPException(status_code=403, detail='Invalid API KEY')
-    df = pd.read_csv(io.BytesIO(await document.read()), header=0, index_col=None, delimiter=';')
-    try:
-        # db_mongo.db('Keywords')['Amazon_keyword_tracker'].insert_many(list(df.T.to_dict().values()))
-        db_mongo.db('Keywords')['tiktok'].insert_many(list(df.T.to_dict().values()), ordered=False)
-    except BulkWriteError:
-        pass
-    return Response(status_code=204)
-
-
-@app.post('/import/tiktoklinks')
-async def import_tiktok(request: Request, document: UploadFile = File(...)):
-    auth_token = request.headers.get('Authorization')
-    if not auth_token or sha256(auth_token.encode('utf-8')).hexdigest() != api_key_hashed or auth_token != api_key:
-        raise HTTPException(status_code=403, detail='Invalid API KEY')
-    df = pd.read_csv(io.BytesIO(await document.read()), header=0, index_col=None, delimiter=';')
-    try:
-        # db_mongo.db('Keywords')['Amazon_keyword_tracker'].insert_many(list(df.T.to_dict().values()))
-        db_mongo.db('Keywords')['tiktoklinks'].insert_many(list(df.T.to_dict().values()), ordered=False)
+        db_mongo.db('Keywords')[collection].insert_many(list(df.T.to_dict().values()), ordered=False)
     except BulkWriteError:
         pass
     return Response(status_code=204)
