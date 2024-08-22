350e57afba875b1172a333e371167e1365c23ba1
Daniil Panov <daniil_panov2005@mail.ru>
2024-08-22 17:36:31 +0300

    hotfix: [h10] can't send data

--- extensions/helium10-extension/background.js
+++ extensions/helium10-extension/background.js
@@ -16,8 +16,8 @@ chrome.runtime.onMessageExternal.addListener(async (message, sender, sendRespons
     )
     if (res.status === 200) {
         sendResponse('OK');
-        res = await startH10(message);
-        if (res) {
+        try {
+            await startH10(message);
             fetch(
                 'http://195.201.194.213:8832/tasks/finish/' + message.task_id,
                 {method: 'PATCH'},
@@ -26,7 +26,7 @@ chrome.runtime.onMessageExternal.addListener(async (message, sender, sendRespons
                 'http://195.201.194.213:8832/tasks/release/' + message.task_id,
                 {method: 'POST'},
             )
-        } else {
+        } catch (e) {
             fetch('http://195.201.194.213:8832/tasks/report/' + message.task_id, {
                 headers: {
                     'Content-Type': 'application/json',
