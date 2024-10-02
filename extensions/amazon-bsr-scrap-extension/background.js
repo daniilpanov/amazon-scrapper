a81603e89511b11e53a1e22d98fc56282c197e02
Daniil Panov <daniil_panov2005@mail.ru>
2024-10-02 23:01:46 +0300

    debug::off; fix: [bsr.ext]

--- extensions/amazon-bsr-scrap-extension/background.js
+++ extensions/amazon-bsr-scrap-extension/background.js
@@ -94,8 +94,7 @@ async function run(task, sender, sendResponse) {
                     return {data: {bsr_url: task.bsr, asins_links: asins_links}};
                 },
             });
-            console.log(result);
-            if (result && result[0]?.result?.data?.asins_links?.length) {
+            if (result && Object.keys(result[0]?.result?.data?.asins_links || {}).length) {
                 break;
             }
             await new Promise(r => setTimeout(r, 100));
