9970f23e5261964ef78c12ee4d069c2df784ee7b
Daniil Panov <daniil_panov2005@mail.ru>
2024-10-10 22:51:34 +0300

    fix: [cmds_router] syntax error

--- server_service/cmds_router.py
+++ server_service/cmds_router.py
@@ -129,7 +129,7 @@ async def finish_bsrtree_cmd(bsr: BSRTreeResult):
     for bsr_url, item in bsr_map.items():
         bsr_url = '/'.join(bsr_url.split('/', maxsplit=3)[3:])
         del item['bsr_link']
-        db('ai_highlights')['departments'].update_one({'URL': bsr_url, 'items': {'$exists': False}, {'$set': {'items': items})
+        db('ai_highlights')['departments'].update_one({'URL': bsr_url, 'items': {'$exists': False}}, {'$set': {'items': items})
     tasks_manager.release_task(bsr.task_id)
     return Response(status_code=HTTP_201_CREATED)
 
