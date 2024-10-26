a7868095ae3d59b7e76d34c7cfda8c59b6a58ea0
Daniil Panov <daniil_panov2005@mail.ru>
2024-10-26 19:23:07 +0300

    feat: Add ProcessController, RecordsController, and Process model with necessary methods

---
+++ extensions/product-targeting-scrap-extension/includes/controllers/ProcessController.js
@@ -0,0 +1,5 @@
+import { Queue } from '../Queue';
+
+class ProcessController {
+    tasksQueue = new Queue();
+}
---
+++ extensions/product-targeting-scrap-extension/includes/models/Process.js
@@ -0,0 +1,55 @@
+class Process {
+    tabId = null;
+    dependencies = [];
+    functions = [];
+
+    addDependency(file) {
+        this.dependencies.push(file);
+    }
+
+    createTab(url) {
+        return chrome.tabs.create({
+            url: url,
+        }, (tab) => { this.tabId = tab.id; });
+    }
+
+    loadDependencies() {
+        return chrome.scripting.executeScript({
+            target: { tabId: this.tabId },
+            files: this.dependencies,
+        });
+    }
+
+    appendFunction(f, ...args) {
+        this.functions.push([f, args]);
+    }
+
+    applyFunctionsAsync() {
+        const res = [];
+        for (const f of this.functions) {
+            res.push(chrome.scripting.executeScript({
+                target: { tabId: this.tabId },
+                args: f[1],
+                func: f[0],
+            }));
+        }
+        return res;
+    }
+
+    async applyFunctionsSync() {
+        const res = [];
+        for (const f of this.functions) {
+            try {
+                const t = await chrome.scripting.executeScript({
+                    target: { tabId: this.tabId },
+                    args: f[1],
+                    func: f[0],
+                });
+                res.push(t?.result[0]);
+            } catch (e) {
+                res.push(e);
+            }
+        }
+        return res;
+    }
+}
