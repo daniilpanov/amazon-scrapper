5f71b302549f05391ef90d417609f505a4cfab86
Daniil Panov <daniil_panov2005@mail.ru>
2024-10-29 22:54:04 +0300

    feat: Add BSRChildDepsParser class with methods for parsing subcategories and links.

---
+++ extensions/product-targeting-scrap-extension/includesInTab/bsrchilddeps.js
@@ -0,0 +1,16 @@
+class BSRChildDepsParser extends MultiParser {
+    elementsQuerySelector = 'div[role=group] > div[role=treeitem]';
+    bsrMP = null;
+
+    getCategoryName(el) {
+        return { bsrLink: el.href || el.getAttribute('href') || null };
+    }
+
+    getLink(el) {
+        return { bsrLink: el.href || el.getAttribute('href') || null };
+    }
+
+    getASINsList() {
+        return (this.MP || (this.MP = new BSRMultiParser())).findASINsList()
+    }
+}
