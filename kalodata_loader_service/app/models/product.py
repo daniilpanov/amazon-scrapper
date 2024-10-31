1ffa22ca6540ad67345affacb40fd526f24055db
Daniil Panov <daniil_panov2005@mail.ru>
2024-10-31 23:46:12 +0300

    feat: first service release
    Added new Pydantic models for Product, Video, and Shop along with their
    respective core metrics. Also established links between products,
    creators, and shops in the DBController.

---
+++ kalodata_loader_service/app/models/product.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+
+
+class ProductCoreMetrics(BaseModel):
+    revenue: float
+    revenue_per_day: float
+    live_revenue: float
+    live_revenue_per_day: float
+    video_mall_revenue: float
+    video_mall_revenue_per_day: float
+    items_sold: float
+    items_sold_per_day: float
+    shopping_mall_revenue: float
+    shopping_mall_revenue_per_day: float
+    avg_unit_price: float
+
+
+class Product(BaseModel):
+    internal_id: str
+    shop_id: int
+    name: str
+    category: str
+    lowest_price_30d: float
+    price_range: list[float]
+    commission_rate: float
+    core_metrics: ProductCoreMetrics
+    creators_ids: list[int]
---
+++ kalodata_loader_service/app/models/shop.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+
+
+class ShopCoreMetrics(BaseModel):
+    revenue: float
+    revenue_per_day: float
+    affiliate_revenue: float
+    affiliate_revenue_per_day: float
+    shopping_mall_revenue: float
+    shopping_mall_revenue_per_day: float
+    items_sold: float
+    items_sold_per_day: float
+    self_operated_account_revenue: float
+    self_operated_account_revenue_per_day: float
+    avg_unit_price: float
+
+
+class SellerOperatedAccount(BaseModel):
+    nickname: str
+    revenue: float
+    followers: float
+
+
+class Shop(BaseModel):
+    internal_id: str
+    name: str
+    type: str
+    core_metrics: ShopCoreMetrics
+    self_operated_accounts: list[SellerOperatedAccount]
