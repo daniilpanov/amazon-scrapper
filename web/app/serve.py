c4b5410c7a33821de5c3b513cae55c6889b52c17
Daniil Panov <daniil_panov2005@mail.ru>
2024-11-18 22:53:09 +0300

    feat: add Dockerfile to the server service and add new web service

---
+++ web/app/serve.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from starlette.responses import FileResponse
+from starlette.staticfiles import StaticFiles
+
+app = FastAPI()
+app.mount('/', StaticFiles(directory='html'), name='static')
+
+
+@app.get('/cp', response_class=FileResponse)
+async def cp_show():
+    return 'index.html'
