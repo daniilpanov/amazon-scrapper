aa899fc086630ead1320383dae26780d94fdc52c
Daniil Panov <daniil_panov2005@mail.ru>
2024-11-19 10:38:59 +0300

    fix: web change static dir

--- web/app/serve.py
+++ web/app/serve.py
@@ -3,7 +3,7 @@ from starlette.responses import FileResponse
 from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
-app.mount('/', StaticFiles(directory='./html'), name='html')
+app.mount('/', StaticFiles(directory='./app/html'), name='html')
 
 
 @app.get('/cp', response_class=FileResponse)
