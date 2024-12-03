2d4ed66f3dc3b88e171f1c9bcfebc7272498b0a2
Daniil Panov <daniil_panov2005@mail.ru>
2024-12-03 15:58:53 +0300

    feat: add opportunity to set the bsrurl in the products task (products_router)

--- server_service/app/products_router.py
+++ server_service/app/products_router.py
@@ -42,6 +42,7 @@ class AsinsCollectingConfig(AmazonTaskConfig):
     current_format: bool = True
     collect_media_config: bool = False
     collect_reviews: bool = True
+    bsrurl: str | None = None
 
 
 class AspectsResultItem(BaseModel):
@@ -100,6 +101,7 @@ async def collect_products_task(config: AsinsCollectingConfig):
         'collect_media_config': config.collect_media_config,
         'target': config.collect_media_config,
         'domain': config.domain,
+        'bsrurl': config.bsrurl,
     }
     data = []
     for item in config.asins:
