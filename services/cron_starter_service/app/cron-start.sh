79dc3ca9adeca7cf33099f2e3006ebf427896956
Daniil Panov <daniil_panov2005@mail.ru>
2025-01-27 10:27:07 +0300

    feat: add unstable template and upgrade the deploy.sh and add new image

--- services/cron_starter_service/app/cron-start.sh
+++ services/cron_starter_service/app/cron-start.sh
@@ -27,7 +27,18 @@ cat << EOF > /etc/logrotate.d/cron_logs
 EOF
 
 # Add logrotate scheduled task to crontab
-echo "0 * * * * /usr/sbin/logrotate /etc/logrotate.d/cron_logs" >> /etc/crontabs/root
+case `grep -Fx "/usr/sbin/logrotate /etc/logrotate.d/cron_logs" "/etc/crontabs/root" >/dev/null; echo $?` in
+  0)
+    echo '> Add the logging task schedule';
+    echo "0 * * * * /usr/sbin/logrotate /etc/logrotate.d/cron_logs" >> /etc/crontabs/root;
+    ;;
+  1)
+    echo '> Logging already scheduled';
+    ;;
+  *)
+    echo '> An error occurred when trying to add the logging task';
+    ;;
+esac
 
 # Start crond and keep it running in the foreground, while outputting logs
 crond
