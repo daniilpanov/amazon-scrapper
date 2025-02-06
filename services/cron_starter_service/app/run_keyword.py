028ba871064b11466eb8411645cc21b408bdd65d
Daniel <daniil_panov2005@mail.ru>
2025-02-07 01:28:10 +0300

    refactor: run_keyword - remove test __main__

--- services/cron_starter_service/app/run_keyword.py
+++ services/cron_starter_service/app/run_keyword.py
@@ -24,15 +24,3 @@ def run_keyword(conn: MongoClient):
     print(group.submit_tasks())
 
 
-if __name__ == '__main__':
-    from dotenv import load_dotenv
-
-    print(load_dotenv('.env') or load_dotenv('../.env') or load_dotenv('../../.env') or load_dotenv('../../../.env'))
-    import certifi
-    from pymongo.server_api import ServerApi
-
-    url = f"{env.get('MONGO_DB_HOST_SCHEMA')}://{env.get('MONGO_DB_USER')}:{env.get('MONGO_DB_PASS')}@{env.get('MONGO_DB_HOST')}"
-    # Create a new client and connect to the server_service
-    with MongoClient(url, server_api=ServerApi('1'), username=env.get('MONGO_DB_USER'),
-                     password=env.get('MONGO_DB_PASS'), tlsCAFile=certifi.where()) as db:
-        run_keyword(db)
