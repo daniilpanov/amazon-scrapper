ca982fff230976b2ed876f0c1a0212a464cda560
Daniil Panov <daniil_panov2005@mail.ru>
2025-02-24 13:50:09 +0300

    add the KWT handler

---
+++ services/msgbroker_handler_service/app/serve.py
@@ -0,0 +1,48 @@
+import inspect
+import os
+from os import environ as env
+import importlib
+import pkgutil
+
+import certifi
+import pika
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+
+import handlers
+
+url = os.environ.get('MONGO_DB_HOST_SCHEMA', 'mongodb') + '://'
+username = None
+password = None
+if 'MONGO_DB_USER' in os.environ:
+    username = os.environ['MONGO_DB_USER']
+    url += username
+    if 'MONGO_DB_PASS' in os.environ:
+        password = os.environ['MONGO_DB_PASS']
+        url += ':' + password
+    url += '@'
+url += os.environ.get('MONGO_DB_HOST', 'localhost')
+
+with (pika.BlockingConnection(pika.ConnectionParameters(env.get('RABBITMQ_HOST', 'localhost'))) as msgbroker,
+    MongoClient(url, server_api=ServerApi('1'), username=username, password=password, tlsCAFile=certifi.where()) as db):
+
+    channel = msgbroker.channel()
+
+    for handler in pkgutil.iter_modules(handlers.__path__):
+        module_path = 'handlers.' + handler.name + '.' + handler.name
+        try:
+            module = importlib.import_module(module_path)
+            params = set(map(lambda i: i[0], inspect.getmembers(module)))
+            if 'Handler' not in params:
+                continue
+        except ModuleNotFoundError as e:
+            print(e)
+            print('Module not found:', module_path)
+            continue
+        except AttributeError as e:
+            print('Module', module_path, 'has no attribute:', e)
+            continue
+        for path, conf in module.Handler(db).handlers.items():
+            channel.basic_consume(path, conf['handler'], auto_ack=conf.get('auto_ack', False), consumer_tag=conf['handler'].__doc__ or None)
+
+    channel.start_consuming()
--- tools/transform-kwt-raw.py
+++ tools/transform-kwt-raw.py
@@ -1,8 +1,10 @@
+import datetime
 import json
 
 with open('resultKWT.json', encoding='UTF-8') as f:
     data = json.loads(f.read())
 
+sq = input('Enter the search query: ')
 new_data = []
 
 
@@ -15,6 +17,7 @@ organic_count = 0
 sponsored_count = 0
 
 for item in filter(lambda i: i, data):
+    item['searchQuery'] = sq
     setdefaultmany(item, ('score', 'price', 'itemPrice', 'oldPrice', 'subscriptionDiscount', 'subscriptionDiscountPercents'), float)
     setdefaultmany(item, ('BestSellerIn', 'image', 'title', 'asin'), str, None)
     item['sponsored_rank'] = None
@@ -32,3 +35,20 @@ for item in filter(lambda i: i, data):
 with open('reskwt.json', 'w', encoding='UTF-8') as f:
     f.write(json.dumps(new_data))
 
+if int(input('Need load this data to database? [1 - yes, 0 - no]')):
+    from dotenv import load_dotenv
+    load_dotenv() or load_dotenv('.env') or load_dotenv('../.env')
+    from os import environ as env
+    import certifi
+    from pymongo import MongoClient
+    from pymongo.server_api import ServerApi
+
+    new_optimized_data = []
+
+    for item in new_data:
+        item['date'] = datetime.datetime.fromisoformat(item['date'])
+        new_optimized_data.append(item)
+
+    url = f"{env.get('MONGO_DB_HOST_SCHEMA')}://{env.get('MONGO_DB_USER')}:{env.get('MONGO_DB_PASS')}@{env.get('MONGO_DB_HOST')}"
+    with MongoClient(url, server_api=ServerApi('1'), username=env.get('MONGO_DB_USER'), password=env.get('MONGO_DB_PASS'), tlsCAFile=certifi.where()) as db_inst:
+        db_inst['Keywords']['keyword_tracking_new'].insert_many(new_optimized_data)
