e130861c2053a3dd5c78866d84c8296afb7c38b0
Daniil Panov <daniil_panov2005@mail.ru>
2025-03-24 12:27:48 +0300

    feat[services/msghandler/kalodata]: add chained start

--- services/msgbroker_handler_service/app/handlers/kalodata/kalodata.py
+++ services/msgbroker_handler_service/app/handlers/kalodata/kalodata.py
@@ -13,9 +13,37 @@ class Handler:
     def __init__(self, db):
         self.db = db
         self.handlers = {
-            'result.success.kwt': {'handler': self.handle_success},
-            'result.error.kwt': {'handler': self.handle_error},
+            'result.success.kalodata': {'handler': self.handle_success},
+            'result.error.kalodata': {'handler': self.handle_error},
         }
+
+    def start_chained_list(self, chan, data, entity):
+        for item in data:
+            chan.basic_publish('tasks', 'kalodata', ('{"destination": "remote", "id": ' + str(item['id']) + ', "entity": "' + entity + '"}').encode(), pika.BasicProperties(delivery_mode=pika.DeliveryMode.Persistent))
+
+    def start_chained_entities(self, chan, data, entity):
+        mapping = {
+            'product': {'searchProducts', 'products', 'product', 'searchNewProducts'},
+            'video': {'video', 'similarVideos'},
+            'creator': {'creator', 'searchCooperativeCreators'},
+            'shop': {'shop', 'searchShopList', 'searchCooperativeShops'},
+        }
+        if entity == 'shop':
+            needle = {'product': mapping['product']}
+        elif entity == 'product':
+            needle = {'video': mapping['video'], 'shop': mapping['shop']}
+        elif entity == 'video':
+            needle = {'creator': mapping['creator']}
+        else:
+            needle = {}
+        for name, keys in needle.items():
+            for key in keys:
+                for item in data.get(key, []):
+                    print('tasks', 'kalodata', ('{"destination": "remote", "id": ' + str(item['id']) + ', "entity": "' + name + '"}').encode())
+                    chan.basic_publish('tasks', 'kalodata', ('{"destination": "remote", "id": ' + str(item['id']) + ', "entity": "' + name + '"}').encode(), pika.BasicProperties(delivery_mode=pika.DeliveryMode.Persistent))
+        if entity == 'video':
+            print('tasks', 'kalodata', ('{"destination": "remote", "id": ' + str(data['details']['creator_id']) + ', "entity": "creator"}').encode())
+            chan.basic_publish('tasks', 'kalodata', ('{"destination": "remote", "id": ' + str(data['details']['creator_id']) + ', "entity": "creator"}').encode(), pika.BasicProperties(delivery_mode=pika.DeliveryMode.Persistent))
 
     def handle_success(self, chan: BlockingChannel, deliver: pika.spec.Basic.Deliver, props, msg):
         data = json.loads(msg.decode())
@@ -30,6 +58,7 @@ class Handler:
                 self.db['Kalodata_new'][entity + 'List'].insert_many([{**item, 'search_query': query, 'scrap_date': date} for item in data], ordered=False)
             except (DuplicateKeyError, BulkWriteError):
                 pass
+            self.start_chained_list(chan, data, entity)
         else:
             try:
                 data['id'] = entity_id
@@ -37,6 +66,7 @@ class Handler:
                 self.db['Kalodata_new'][entity].insert_one(data)
             except (DuplicateKeyError, BulkWriteError):
                 pass
+            self.start_chained_entities(chan, data, entity)
 
         chan.basic_ack(deliver.delivery_tag)
 
