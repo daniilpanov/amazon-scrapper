8b66ad01ec154df091fe4846f778a83f0a27c9ca
Daniil Panov <daniil_panov2005@mail.ru>
2025-03-29 17:19:15 +0300

    refactor[services/server]: remove old comments

--- services/server_service/app/cmds_router.py
+++ services/server_service/app/cmds_router.py
@@ -53,7 +53,7 @@ async def collect_products_form(config: CollectProductsForm):
         alias=config.alias,
         asins=[products_router.AsinsItemCollectConfig(
             asin=asin,
-            collect_media_config=asin in target,  # if asin=target then collect media
+            collect_media_config=asin in target,
         ) for asin in asins],
         collect_aspects=config.collect_aspects,
         collect_reviews=config.collect_reviews,
@@ -146,7 +146,7 @@ async def category_set_cmd(data=Body()):
     try:
         operations = [
             pymongo.UpdateOne(
-                {'Category': data['cat_name'], 'ASIN': asin},  # Фильтр для поиска существующих записей
+                {'Category': data['cat_name'], 'ASIN': asin},
                 {
                     '$set': {
                         'relation_to_category': client_name if asin in target else None,
@@ -159,12 +159,6 @@ async def category_set_cmd(data=Body()):
         ]
         if operations:
             db('amazon_data')['all_categories'].bulk_write(operations)
-        # db('amazon_data')['all_categories'].insert_many([{
-        #     'Category': data['cat_name'],
-        #     'ASIN': asin,
-        #     'relation_to_category': client_name if asin in target else None,
-        #     'relation_to_TOP5': asin in top5_asins,
-        # } for asin in asins])
         return Response(status_code=HTTP_201_CREATED)
     except BulkWriteError:
         pass
