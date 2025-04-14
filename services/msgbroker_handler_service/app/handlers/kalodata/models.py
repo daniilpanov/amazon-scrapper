6bdb8134a504199ff4a8db42e638e94e8bb87cbe
Daniil Panov <daniil_panov2005@mail.ru>
2025-04-14 16:49:56 +0300

    temp

---
+++ services/msgbroker_handler_service/app/handlers/kalodata/models.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+
+
+class BrandMetrics(BaseModel):
+    pass
+
+class ProductMetrics(BaseModel):
+    pass
+
+class CreatorMetrics(BaseModel):
+    pass
+
+class VideoMetrics(BaseModel):
+    pass
+
+
+class Brand(BaseModel):
+    pass
+
+class Product(BaseModel):
+    pass
+
+class Video(BaseModel):
+    pass
+
+class Creator(BaseModel):
+    pass
