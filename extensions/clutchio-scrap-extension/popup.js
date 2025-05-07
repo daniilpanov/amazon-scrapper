9ce8fcc8e6b050f04a3fbc7ebc1c2f69138354ab
Daniil Panov <daniil_panov2005@mail.ru>
2025-05-08 01:56:20 +0300

    refactor[ext]: remove unused

--- extensions/clutchio-scrap-extension/popup.js
+++
@@ -1,9 +0,0 @@
-const grabBtn = document.getElementById("grabBtn");
-grabBtn.addEventListener("click", () => {
-    chrome.tabs.create({
-        url: "https://clutch.co/agencies/ecommerce-marketing",
-        active: false,
-    }, function (tab) {
-        chrome.runtime.sendMessage(tab.id);
-    });
-});
--- extensions/product-targeting-scrap-extension/includes/controllers/RecordsController.js
+++
@@ -1,3 +0,0 @@
-class RecordsController {
-
-}
