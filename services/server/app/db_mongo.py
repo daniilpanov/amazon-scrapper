4ef3f1b063ad83d387f42c33cb3f959349aa75b1
Daniil Panov <daniil_panov2005@mail.ru>
2025-10-10 14:48:02 +0300

    refactor: Global renaming

--- services/server/app/db_mongo.py
+++ services/server/app/db_mongo.py
@@ -34,7 +34,7 @@ def init(username=None, password=None):
     if 'proxy' in config and config['proxy']:
         import os
         os.environ['MONGO_PROXY'] = config['proxy']
-    # Create a new client and connect to the server_service
+    # Create a new client and connect to the server
     client = MongoClient(url, server_api=ServerApi('1'), username=username, password=password, tlsCAFile=certifi.where())
     # Send a ping to confirm a successful connection
     try:
