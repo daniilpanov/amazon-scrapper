1fec6ea905fb7592c3fa28b29f09b5bbcf805da2
Daniil Panov <daniil_panov2005@mail.ru>
2025-10-23 12:31:56 +0300

    feat[msgbroker_handler/asinsight]: implement gathering logic

--- services/msgbroker_handler/app/startup.py
+++ services/msgbroker_handler/app/startup.py
@@ -94,6 +94,7 @@ def startup(logger):
         declare_tasks_queue('products')
         declare_tasks_queue('bsr')
         declare_tasks_queue('helium_blackbox')
+        declare_tasks_queue('asinsight')
         declare_tasks_queue('kwt', { 'x-max-priority': 20 })
 
         ### RESULTS ###
