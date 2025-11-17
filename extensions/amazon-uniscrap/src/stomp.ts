339f55775895efac98b4811cbd0413eb6f0cdd13
Daniil Panov <daniil_panov2005@mail.ru>
2025-11-17 13:56:36 +0300

    improve[uniscrap/stomp.ts]

--- extensions/amazon-uniscrap/src/stomp.ts
+++ extensions/amazon-uniscrap/src/stomp.ts
@@ -1,4 +1,4 @@
-import { CompatClient, Stomp, StompSubscription } from '@stomp/stompjs';
+import { CompatClient, IMessage, Stomp, StompSubscription } from '@stomp/stompjs';
 
 type TaskResult = void | boolean | Promise<void> | Promise<boolean>;
 type TaskHandler = (task: any) => TaskResult;
@@ -34,67 +34,74 @@ export default class StompConnection {
 
     public connect(err?: any): void | Promise<void> {
         if (!this.needReconnect || this.reconnectionCount >= this.maxReconnectionCount) {
-            if (this.resetNeedReconnect) this.needReconnect = true;
+            if (this.resetNeedReconnect)
+                this.needReconnect = true;
+
             this.reconnectionCount = 0;
             return;
         }
+
         ++this.reconnectionCount;
 
         if (this.connected) {
             try {
                 this.stompClient!.disconnect();
-            } catch (e) {
-            }
+            } catch (e) {}
         }
 
         this.stompClient = Stomp.over(() => new WebSocket(this.sockURL));
         this.stompClient.onStompError = this.connect.bind(this);
 
         return new Promise(async (resolve, reject): Promise<void> => {
-            if (err) console.log('Reconnecting due to the error:', err);
+            if (err)
+                console.log('Reconnecting due to the error:', err);
 
-            if (this.stompClient) {
-                this.stompClient.connect(
-                    { login: 'guest', passcode: 'guest' },
-                    (frame: any) => {
-                        console.log('Connected: ' + frame);
-                        this.reconnectionCount = 0;
-                        this.stompClient?.activate();
-
-                        this.subscriptions.forEach((config: TaskConfig, queue: string) => {
-                            if (config.active) {
-                                this.subscribe(queue, config.func, undefined, config.params);
-                            }
-                        });
-
-                        return resolve(frame);
-                    },
-                    (error: any) => {
-                        console.error('Connection error: ' + error);
-                        return reject(error);
-                    },
-                );
+            if (!this.stompClient) {
+                console.error('Connection error: no STOMP');
+                return reject();
             }
+
+            this.stompClient.connect(
+                { login: 'guest', passcode: 'guest' },
+                (frame: any) => {
+                    console.log('Connected: ' + frame);
+                    this.reconnectionCount = 0;
+                    this.stompClient?.activate();
+
+                    this.subscriptions.forEach((config: TaskConfig, queue: string) => {
+                        if (config.active)
+                            this.subscribe(queue, config.func, undefined, config.params);
+                    });
+
+                    return resolve(frame);
+                },
+                (error: any) => {
+                    console.error('Connection error: ' + error);
+                    return reject(error);
+                },
+            );
         });
     }
 
-    public subscribe(queue: string, callback: TaskHandler, prefetch_count?: number, params?: {
-        [key: string]: string,
-    }): void {
-        if (!params) params = {};
-        if (!prefetch_count) prefetch_count = this.maxConcurrentTasks;
+    public subscribe(queue: string, callback: TaskHandler, prefetch_count?: number, params?: {[key: string]: string}) {
+        if (!params)
+            params = {};
+
+        if (!prefetch_count)
+            prefetch_count = this.maxConcurrentTasks;
+
         params['durable'] = params['durable'] ?? 'true';
         params['ack'] = params['ack'] ?? 'client-individual';
         params['prefetch-count'] = params['prefetch-count'] ?? prefetch_count;
 
-        // Если подписка уже существует, обновляем ее
+        // If a subscription exists, update it
         if (this.subscriptions.has(queue)) {
             const existingConfig = this.subscriptions.get(queue)!;
             existingConfig.func = callback;
             existingConfig.params = { ...params };
             existingConfig.active = true;
 
-            // Если клиент подключен и подписка неактивна, создаем новую
+            // If a client connected, but a subscription is inactive, then creates a new one
             if (this.stompClient?.connected && !existingConfig.subscription) {
                 existingConfig.subscription = this.stompClient.subscribe(
                     queue,
@@ -105,7 +112,7 @@ export default class StompConnection {
             return;
         }
 
-        // Создаем новую подписку
+        // Create a new subscription
         const newConfig: TaskConfig = {
             func: callback,
             params: { ...params },
@@ -124,8 +131,9 @@ export default class StompConnection {
         }
     }
 
-    public unsubscribe(queue: string): void {
-        if (!this.subscriptions.has(queue)) return;
+    public unsubscribe(queue: string) {
+        if (!this.subscriptions.has(queue))
+            return;
 
         const config = this.subscriptions.get(queue)!;
         config.active = false;
@@ -137,22 +145,21 @@ export default class StompConnection {
     }
 
     public updateSubscription(queue: string, active: boolean): void {
-        if (active) {
-            const config = this.subscriptions.get(queue);
-            if (config && !config.subscription && this.stompClient?.connected) {
-                config.subscription = this.stompClient.subscribe(
-                    queue,
-                    (task: any) => this.handleMessage(task, config.func),
-                    config.params,
-                );
-                config.active = true;
-            }
-        } else {
-            this.unsubscribe(queue);
+        if (!active)
+            return this.unsubscribe(queue);
+
+        const config = this.subscriptions.get(queue);
+        if (config && !config.subscription && this.stompClient?.connected) {
+            config.subscription = this.stompClient.subscribe(
+                queue,
+                (task: any) => this.handleMessage(task, config.func),
+                config.params,
+            );
+            config.active = true;
         }
     }
 
-    private async handleMessage(message: any, callback: TaskHandler): Promise<void> {
+    private async handleMessage(message: IMessage, callback: TaskHandler): Promise<void> {
         this.reconnectionCount = 0;
 
         let data = message.body;
@@ -160,36 +167,35 @@ export default class StompConnection {
             data = JSON.parse(data);
         } catch (e) {}
 
-        console.log('Received message:', data);
-
-        if (this.onTaskStarted && !this.onTaskStarted(data)) {
+        if (this.onTaskStarted && !this.onTaskStarted(data))
             return message.nack();
-        }
+
         try {
-            // Вызываем callback для обработки сообщения
-            let res;
-            res = await callback.bind(this)(data);
-            if ((typeof res === 'undefined' || res) && (!this.onTaskFinished || this.onTaskFinished(data))) {
+            const res = await callback.bind(this)(data);
+            if ((typeof res === 'undefined' || res) && (!this.onTaskFinished || this.onTaskFinished(data)))
                 message.ack();
-            } else {
+            else
                 message.nack();
-            }
         } catch (e) {
             console.error('Error while processing a task:', message, e);
-            message.nack();
+            message.nack({ requeue: "false" });
         }
     }
 
     public sendMessage(queue: string, message: any, headers?: { [key: string]: string }): void {
-        if (!this.stompClient) return;
-        if (!headers) headers = {};
+        if (!this.stompClient)
+            return;
+
+        if (!headers)
+            headers = {};
 
         try {
-            if (typeof message === 'string') this.stompClient.publish({
-                destination: queue,
-                headers: headers,
-                body: message,
-            });
+            if (typeof message === 'string')
+                this.stompClient.publish({
+                    destination: queue,
+                    headers: headers,
+                    body: message,
+                });
             else if (typeof message === 'object') {
                 try {
                     this.stompClient.publish({
@@ -207,14 +213,19 @@ export default class StompConnection {
             }
         } catch (e) {
             console.log(e);
+            console.error(e);
         }
     }
 
     public disconnect(): void {
-        if (!this.stompClient) return;
+        if (!this.stompClient)
+            return;
+
         this.subscriptions.forEach(({ active, subscription }) => {
-            if (active && subscription) subscription.unsubscribe();
+            if (active && subscription)
+                subscription.unsubscribe();
         });
+
         this.stompClient.disconnect();
     }
 }
